035171d46567e02111c58bf1ae81c510a0a7e545
javier <johnnybelesaca@gmail.com>
2021-04-23 13:21:01 -0400

    Task: Part10 configured SQLalchemy

--- app/config.py
+++ app/config.py
@@ -16,9 +16,20 @@ class Config:
     FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
 
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-Assets
+    LESS_BIN = environ.get('LESS_BIN')
+    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
+    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+
     # Static Assets
-    STATIC_FOLDER = "static"
-    TEMPLATES_FOLDER = "templates"
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
 
 class ProdConfig(Config):
     FLASK_ENV = 'production'
